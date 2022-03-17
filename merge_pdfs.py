8f04abf3491bac1cd6e5e9b14f6ecda73cc6ea30
nigginoki <100469720+nigginoki@users.noreply.github.com>
2022-03-17 22:58:14 +0100

    Add files via upload

---
+++ merge_pdfs.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 17 12:18:29 2021
+
+@author: nicholas
+"""
+import tkinter 
+from tkinter import filedialog
+from PyPDF2 import PdfFileReader, PdfFileWriter
+
+def merge_pdfs(result_name):
+    pdf_writer = PdfFileWriter()
+    root = tkinter.Tk()
+    files = filedialog.askopenfilenames(parent=root,title='PDFs auswählen!')
+    for file in root.tk.splitlist(files):
+        pdf_reader = PdfFileReader(file)
+        for page in range(pdf_reader.getNumPages()):
+            pdf_writer.addPage(pdf_reader.getPage(page))
+    print("PDFs zusammengefügt.")
+    with open(result_name, 'wb') as out:
+        pdf_writer.write(out)
+        print("PDF-Datei erfolgreich erstellt.")
+merge_pdfs('merged.pdf')
