5d0d5ebe90c0035fc688af4bc285b6ef7cac4884
nigginoki <100469720+nigginoki@users.noreply.github.com>
2023-02-01 14:46:46 +0100

    Update merge_pdfs.py

--- merge_pdfs.py
+++ merge_pdfs.py
@@ -12,13 +12,12 @@ from PyPDF2 import PdfFileReader, PdfFileWriter
 def merge_pdfs(result_name):
     pdf_writer = PdfFileWriter()
     root = tkinter.Tk()
-    files = filedialog.askopenfilenames(parent=root,title='PDFs auswählen!')
+    files = filedialog.askopenfilenames(parent=root,title='Choose PDFs !')
     for file in root.tk.splitlist(files):
         pdf_reader = PdfFileReader(file)
         for page in range(pdf_reader.getNumPages()):
             pdf_writer.addPage(pdf_reader.getPage(page))
-    print("PDFs zusammengefügt.")
     with open(result_name, 'wb') as out:
         pdf_writer.write(out)
-        print("PDF-Datei erfolgreich erstellt.")
+        print("PDF-Datei created sucessfully.")
 merge_pdfs('merged.pdf')
